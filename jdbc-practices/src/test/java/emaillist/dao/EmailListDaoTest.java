9b2760d270ed1341078111088b4b0c1fac1e635e
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-05-17 17:22:26 +0900

    EmaillistDao #2

--- jdbc-practices/src/test/java/emaillist/dao/EmailListDaoTest.java
+++ jdbc-practices/src/test/java/emaillist/dao/EmailListDaoTest.java
@@ -1,7 +1,12 @@
 package emaillist.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -11,7 +16,14 @@ import emaillist.vo.EmaillistVo;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmailListDaoTest {
+	private static int count = 0;
 
+	@BeforeAll
+	public static void setUp() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		count = list.size();
+	}
+	
 	@Test
 	@Order(1)
 	public void testInsert() {
@@ -26,8 +38,19 @@ public class EmailListDaoTest {
 	
 	@Test
 	@Order(2)
+	public void testFindAll() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		assertEquals(count + 1, list.size());
+	}
+	
+	@Test
+	@Order(3)
 	public void testDeleteByEmail() {
 		boolean result = new EmaillistDao().deleteByEmail("dooly@gmail.com");
 		assertTrue(result);
+	}
+
+	@AfterAll
+	public static void cleanup() {
 	}
 }
